d6308e30dd47c795de2ec6c80521687b92c3f5ac
mila25-25 <liudmyla.hoshko@gmail.com>
2026-03-05 09:37:49 +0100

    5.03 9:37

--- BudgetManager.Tests/ReportServiceTests.cs
+++ BudgetManager.Tests/ReportServiceTests.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
-using BudgetManager.Models;
+﻿using BudgetManager.Models;
 using BudgetManager.Services;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace BudgetManager.Tests
@@ -8,7 +9,7 @@ namespace BudgetManager.Tests
     public class ReportServiceTests
     {
         [Fact]
-        public void CalculateBalance_ShouldReturnCorrectBalance()
+        public void Balance_ShouldBeCorrect_AfterAddingIncomeAndExpense()
         {
             var service = new ReportService();
 
@@ -21,6 +22,84 @@ namespace BudgetManager.Tests
             var result = service.CalculateBalance(transactions);
 
             Assert.Equal(800m, result);
+        }
+
+        [Fact]
+        public void Balance_ShouldBeZero_WhenNoTransactions()
+        {
+            var service = new ReportService();
+
+            var transactions = new List<Transaction>();
+
+            var result = service.CalculateBalance(transactions);
+
+            Assert.Equal(0m, result);
+        }
+
+        [Fact]
+        public void MonthlyReport_ShouldSumIncomeCorrectly()
+        {
+            var service = new ReportService();
+
+            var transactions = new List<Transaction>
+            {
+                new Transaction { Amount = 1000m, IsIncome = true, Date = new DateTime(2025, 3, 1) },
+                new Transaction { Amount = 500m, IsIncome = true, Date = new DateTime(2025, 3, 5) },
+                new Transaction { Amount = 200m, IsIncome = false, Date = new DateTime(2025, 3, 10) }
+            };
+
+            var result = service.CalculateMonthlyIncome(transactions, new DateTime(2025, 3, 1));
+
+            Assert.Equal(1500m, result);
+        }
+
+        [Fact]
+        public void MonthlyReport_ShouldSumExpensesCorrectly()
+        {
+            var service = new ReportService();
+
+            var transactions = new List<Transaction>
+            {
+                new Transaction { Amount = 100m, IsIncome = false, Date = new DateTime(2025, 3, 1) },
+                new Transaction { Amount = 50m, IsIncome = false, Date = new DateTime(2025, 3, 3) }
+            };
+
+            // ВАЖНО: расходы считаем отдельным методом
+            var result = service.CalculateMonthlyExpenses(transactions, new DateTime(2025, 3, 1));
+
+            Assert.Equal(150m, result);
+        }
+
+        [Fact]
+        public void MonthlyReport_ShouldIgnoreOtherMonths()
+        {
+            var service = new ReportService();
+
+            var transactions = new List<Transaction>
+            {
+                new Transaction { Amount = 1000m, IsIncome = true, Date = new DateTime(2025, 3, 1) },
+                new Transaction { Amount = 500m, IsIncome = true, Date = new DateTime(2025, 4, 1) }
+            };
+
+            var result = service.CalculateMonthlyIncome(transactions, new DateTime(2025, 3, 1));
+
+            Assert.Equal(1000m, result);
+        }
+
+        [Fact]
+        public void MonthlyExpenses_ShouldIgnoreOtherMonths()
+        {
+            var service = new ReportService();
+
+            var transactions = new List<Transaction>
+            {
+                new Transaction { Amount = 100m, IsIncome = false, Date = new DateTime(2025, 3, 10) },
+                new Transaction { Amount = 200m, IsIncome = false, Date = new DateTime(2025, 4, 10) }
+            };
+
+            var result = service.CalculateMonthlyExpenses(transactions, new DateTime(2025, 3, 1));
+
+            Assert.Equal(100m, result);
         }
     }
 }
